440c436ead0e843a46f7fcadb1e1d49a838df9ad
HAFSA AALLAT <haallat@sfer.in.adc.education.fr>
2024-02-05 10:31:48 +0100

    update

--- code_utils/utils.py
+++ code_utils/utils.py
@@ -1,6 +1,8 @@
 def aplatir(conteneurs):
     return [conteneurs[i][j] for i in range(len(conteneurs)) for j in range(len(conteneurs[i]))]
 
-
-def dict_wg_chap_freq(row):
-    for i in range(len(row['chap'])):
+def wg_chap_to_dict(row):
+    list_chap=[]
+    for i in range(len(row['wg'])):
+        list_chap.append({'name': row['chap'][i], 'wg': row['wg'][i].replace('wg',''), 'chap': int(row['chap'][i][-2:].replace('_','')), 'freq_ipcc': row['freq']})
+    return list_chap
