02aeb0fa3dc850518003223a3796b9efeacc85ee
hfsllt <hafsa.aallat@gmail.com>
2024-02-09 22:50:30 +0100

    update

--- code_utils/pickle.py
+++ code_utils/pickle.py
@@ -2,9 +2,9 @@ import pandas as pd
 import pickle
 
 def write_cache(cached_openalex_data,path):
-    pickle.dump(cached_openalex_data, open(path+'cached_openalex_data.pkl', 'wb'))
+    pickle.dump(cached_openalex_data, open(path+f'{cached_openalex_data}.pkl', 'wb'))
 
 def load_cache(cached_openalex_data,path):
-    cached_openalex_data = pickle.load(open(path+'cached_openalex_data.pkl', 'rb'))
+    cached_openalex_data = pickle.load(open(path+f'{cached_openalex_data}.pkl', 'rb'))
     print(f'{len(cached_openalex_data)} data in cached openalex data')
     return cached_openalex_data
